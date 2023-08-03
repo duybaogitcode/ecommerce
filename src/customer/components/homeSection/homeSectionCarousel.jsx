9f41fe5353bc3d5cdcc7d87cd3d5876fad798837
Duy Bao <bao1322002@gmail.com>
2023-08-03 11:45:17 +0700

    feat: add arrow btn

--- src/customer/components/homeSection/homeSectionCarousel.jsx
+++ src/customer/components/homeSection/homeSectionCarousel.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import HomeSectionCard from './homeSectionCard';
+import { Button } from '@mui/material';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 function HomeSectionCarousel() {
   const responsive = {
@@ -12,15 +15,22 @@ function HomeSectionCarousel() {
   const items = [1, 1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard key={item} />);
   return (
     <div className='flex items-center justify-center'>
+      <Button variant='contained' className='z-50' sx={{ position: 'absolute', left: '0rem' }}>
+        <KeyboardArrowLeftIcon></KeyboardArrowLeftIcon>
+      </Button>
       <AliceCarousel
         mouseTracking
         items={items}
+        disableDotsControls
         disableButtonsControls
         infinite
         autoPlay
         autoPlayInterval={2000}
         responsive={responsive}
       />
+      <Button variant='contained' className='z-50' sx={{ position: 'absolute', right: '0rem' }}>
+        <KeyboardArrowRightIcon></KeyboardArrowRightIcon>
+      </Button>
     </div>
   );
 }
