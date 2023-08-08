4f21b48e0cd3788a4c8b37d39c0af6d77e58c41a
Duy Bao <bao1322002@gmail.com>
2023-08-09 00:55:57 +0700

    feat: adding price

--- src/customer/components/homeSection/homeSectionCard.js
+++ src/customer/components/homeSection/homeSectionCard.js
@@ -10,12 +10,15 @@ export default function HomeSectionCard({ item }) {
         <h3 className='text-lg font-medium line-clamp-1'>{item.title}</h3>
         <p className='mt-2 text-sm line-clamp-3'>{item.description}</p>
       </div>
-      <a
-        className='rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800'
-        href='#'
-      >
-        <p>Add to cart</p>
-      </a>
+      <div className='flex items-center justify-between w-full px-4 py-2'>
+        <p className='text-lg font-medium'>${item.price}</p>
+        <a
+          className='rounded-lg bg-cyan-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800'
+          href='#'
+        >
+          Add to cart
+        </a>
+      </div>
     </div>
   );
 }
